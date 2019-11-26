dbdec8e9739d6f15993a772276f1b6e84faca190
Chris Kiefer <chrisk13fer@gmail.com>
2019-11-26 16:31:40 +0100

    timeout, peer list

--- sema_ticks.js
+++ sema_ticks.js
@@ -5,12 +5,13 @@ console.log("Sema_ticks")
 
 var args = process.argv.slice(2);
 
+var peers = {};
 
 var PORT = 7243;
 var netInterface = args.length > 1 ? args[1] : "en0"
 var BROADCAST_ADDR = broadcastAddress(netInterface);
 var dgram = require('dgram');
-var server = dgram.createSocket("udp4");
+var server = dgram.createSocket({type:"udp4", reuseAddr:true}); //todo: shared socket https://nodejs.org/api/dgram.html addMembership
 var hellomessage = Buffer.from("hi i'm " + args[0] + ".");
 var hiRegex = /hi i'm [A-Za-z0-9]+\./;
 
@@ -23,10 +24,18 @@ function broadcastNew() {
     server.send(hellomessage, 0, hellomessage.length, PORT, BROADCAST_ADDR, function() {
         console.log("Sent '" + hellomessage + "'");
     });
+    for(p in peers) {
+      console.log(p);
+      console.log(peers[p]);
+      peers[p] = peers[p] - 1;
+      if (peers[p] == 0) {
+        delete peers[p];
+      }
+    }
 }
 
 
-var client = dgram.createSocket('udp4');
+var client = dgram.createSocket({type:"udp4", reuseAddr:true});
 
 client.on('listening', function () {
     var address = client.address();
@@ -40,6 +49,7 @@ client.on('message', function (message, rinfo) {
     var clockName = match[0].substr(7,match[0].length-8);
     if (match != null) {
       console.log("message match: " + clockName );
+      peers[clockName] = 10; //timeout in secs
     }
 });
 
