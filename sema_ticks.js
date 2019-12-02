01f492058f2e7496fa56248518fb22e3259db69d
Chris Kiefer <chrisk13fer@gmail.com>
2019-12-02 17:01:57 +0100

    debug remove

--- sema_ticks.js
+++ sema_ticks.js
@@ -100,7 +100,7 @@ client.on('message', function(message, rinfo) {
     }
   } else if (msgdata.c == "p") {
     if (msgdata.id != machineName) {
-      console.log("phase recv: " + msgdata.p);
+      // console.log("phase recv: " + msgdata.p);
       // console.log(peers);
       // console.log(wsclient);
       if (wsclient != null) {
